b66f0a2972ff0a17c69ad438505865619662f011
Kevin Sheedy <kevin.x.sheedy@aib.ie>
2015-05-19 12:56:32 +0100

    Sample repos and screenshots

--- samples.sh
+++ samples.sh
@@ -27,7 +27,34 @@ git commit -am "Second Commit"
 
 # Feature Branches
 git branch feature/dashboard-screen
+git branch feature/customer-screen
 git branch feature/login-screen
+
+#
+git checkout feature/dashboard-screen
+
+# Dashboard Feature Commit
+echo "lorem ipsum" > dashboard.html
+git add -A
+git commit -am "Dashboard screen"
+
+# Another Dashboard Feature Commit
+echo "lorem ipsum" > dashboard.js
+git add -A
+git commit -am "Dashboard screen bugfix"
+
+#
+git checkout feature/customer-screen
+
+# Customer Feature Commit
+echo "lorem ipsum" > customer.html
+git add -A
+git commit -am "Customer screen"
+
+# Another Customer Feature Commit
+echo "lorem ipsum" > customer.js
+git add -A
+git commit -am "Customer screen enhancement"
 
 # Feature Branch
 git checkout feature/login-screen
@@ -43,39 +70,7 @@ git add -A
 git commit -am "Login screen validation"
 
 
-#
-git checkout feature/dashboard-screen
-
-# Dashboard Feature Commit
-echo "lorem ipsum" > Dashboard.html
-git add -A
-git commit -am "Dashboard screen"
-
-# Another Dashboard Feature Commit
-echo "lorem ipsum" > Dashboard.js
-git add -A
-git commit -am "Dashboard screen validation"
-
-
 # Merge the feature branch
 git checkout release/1.0.0
-#git merge feature/login-screen --no-ff
+git merge feature/login-screen --no-ff
 
-git status
-git status
-git status
-git status
-git status
-git status
-git status
-git status
-git status
-git status
-git status
-git status
-git status
-
-# Prepare the third commit
-echo "lorem ipsum" > foobar.txt
-git add -A
-git commit -am "Third Commit"
